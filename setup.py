e4131a18ff6c539f43b6c22fa7f6b9a052d0ebcd
Masamitsu MURASE <masamitsu.murase@gmail.com>
2020-07-23 23:16:47 +0900

    Version 2020.2.117

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     author="Masamitsu MURASE",
     author_email="masamitsu.murase@gmail.com",
     url="https://github.com/masamitsu-murase/openvino-rt",
-    version="2020.1.033",
+    version="2020.2.117",
     description="OpenVINO Runtime Libraries",
     zip_safe=False,
     install_requires=["icc_rt"],
